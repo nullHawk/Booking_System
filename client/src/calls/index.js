192efc3e03f8d1c9fd9603e734cdafe677833b92
Suryansh Shakya <suryanshsinghshakya1@gmail.com>
2024-07-13 14:41:11 +0530

    minor changes

--- client/src/calls/index.js
+++ client/src/calls/index.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 export const axiosInstance = axios.create({
     headers : {
         'Content-Type' : 'application/json',
+        'authorzation' : `Bearer ${localStorage.getItem("token")}`
        
     }
 })
